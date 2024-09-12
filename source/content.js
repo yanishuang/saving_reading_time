0de5e1ea8129038bab46b2a61faf6da2c792e6e0
yanishuang <2452662575@qq.com>
2024-09-12 14:51:53 +0800

    删除source

--- source/content.js
+++
@@ -1,8 +0,0 @@
-// 这个文件目前为空,因为我们不需要在页面中注入任何脚本
-// 如果将来需要与页面交互,可以在这里添加代码
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "openPopup") {
-    chrome.runtime.sendMessage({ action: "openPopup" });
-  }
-});
